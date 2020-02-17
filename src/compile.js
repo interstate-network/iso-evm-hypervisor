aaa8332171152bcb3e408c2d4c8794ce87fbc35a
dillon <dillonkellar@protonmail.com>
2020-02-16 21:55:57 -0600

    removed empty lines

--- src/compile.js
+++ src/compile.js
@@ -2,4 +2,4 @@ const path = require('path');
 const compileHuff = require('./lib/easy-huff')
 const vmPath = path.join(__dirname, 'huff_modules');
 
-module.exports = compileHuff(vmPath, 'hypervisor.huff', 'INITIALIZE_HYPERVISOR');;
+module.exports = compileHuff(vmPath, 'hypervisor.huff', 'INITIALIZE_HYPERVISOR');
